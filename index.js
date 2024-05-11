8a858b305f97f724c9685adafab99d380f81fd83
Đặng Tiến Thành <thanhdang@asia.icsc.vn>
2024-05-12 01:19:07 +0700

    Cor update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://eventdot.netlify.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
     optionsSuccessStatus: 200,
