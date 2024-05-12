44528d671db75cc998a3c1cbc0897b20dfdedc8b
Đặng Tiến Thành <thanhdang@asia.icsc.vn>
2024-05-13 01:58:17 +0700

    fix

--- Middlewares/AuthMiddleware.js
+++ Middlewares/AuthMiddleware.js
@@ -6,11 +6,11 @@ const jwt = require("jsonwebtoken");
 module.exports.userVerification = (req, res) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return res.json({ status: false });
+    return res.json({ status: false, message: "Missing token!" });
   }
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
-      return res.json({ status: false });
+      return res.json({ status: false, message: "Cookie verify fail!" });
     } else {
       const user = await User.findById(data.id);
       if (user) {
