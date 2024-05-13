880261667128e152fe20a1f1ede0f1768b3b2b01
Đặng Tiến Thành <thanhdang@asia.icsc.vn>
2024-05-13 10:18:45 +0700

    fix join event

--- Controllers/EventController.js
+++ Controllers/EventController.js
@@ -55,7 +55,7 @@ module.exports.UserJoinEvent = async (req, res) => {
   const userUpdate = { $push: { soloParticipants: userId } };
   // const groupUdate = { $push: { groups: groupId } };
 
-  const existEvent = Event.findOne({ soloParticipants: userId });
+  const existEvent = Event.findOne({ userId: { $in: soloParticipants } });
 
   if (!userId) {
     res.json({
